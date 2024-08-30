e9adba5f7ec2b239a0b253d0cd2426909f4192c5
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-08-30 15:19:15 +0100

    Update RoomComponent

--- components/RoomsComponents/RoomComponent.tsx
+++ components/RoomsComponents/RoomComponent.tsx
@@ -8,16 +8,15 @@ import clsx from "clsx";
 import { link as linkStyles } from "@nextui-org/theme";
 import { Button, Divider, RangeCalendar } from "@nextui-org/react";
 import { getRoomByID } from "@/app/action";
-import { MapComponent } from "./MapComponent";
 import { Loader } from "../loader";
 import { useState } from "react";
 import { today, getLocalTimeZone } from '@internationalized/date';
 import NextLink from "next/link";
 
-
 // Dynamically import the MapContainer component to prevent SSR
 const DynamicMap = dynamic(() => import("./MapComponent").then((mod) => mod.MapComponent), {
     ssr: false,
+    loading: () => <div>Loading map...</div>, // Optional: Add loading state
 });
 
 export const RoomComponent = ({ _id }: RoomComponentProps) => {
@@ -26,7 +25,7 @@ export const RoomComponent = ({ _id }: RoomComponentProps) => {
         queryFn: () => getRoomByID(_id),
     });
 
-    let [value, setValue] = useState({
+    const [value, setValue] = useState({
         start: today(getLocalTimeZone()),
         end: today(getLocalTimeZone()).add({ weeks: 1 }),
     });
@@ -36,127 +35,113 @@ export const RoomComponent = ({ _id }: RoomComponentProps) => {
     }
 
     if (error) {
-        console.error('Error fetching room:', error); // Detailed logging
-        return <h2>{error.message}</h2>;
+        console.error('Error fetching room:', error);
+        return <Typography variant="h4" color="error">Error loading room data.</Typography>;
     }
 
-    if (room) {
-        return (
-            <>
-                {/* Title AREA */}
-                <Grid2 justifyContent={"center"} container marginY={2}>
-                    <Typography className={clsx(
-                        linkStyles({ color: "foreground" }),
-                        "!text-4xl md:!text-5xl",
-                        "mb-2"
-                    )} variant="h3">
-                        {room.title}
-                    </Typography>
-                </Grid2>
+    if (!room) {
+        // Return a fallback UI or null if room data is not available
+        return <Typography variant="h4">Room not found.</Typography>;
+    }
 
-                {/* Carousel AREA */}
-                <Grid2 container justifyContent={"center"} marginY={2}>
-                    <Caroussel Photos={room.photos} />
-                </Grid2>
+    return (
+        <>
+            {/* Title AREA */}
+            <Grid2 justifyContent={"center"} container marginY={2}>
+                <Typography className={clsx(
+                    linkStyles({ color: "foreground" }),
+                    "!text-4xl md:!text-5xl",
+                    "mb-2"
+                )} variant="h3">
+                    {room.title}
+                </Typography>
+            </Grid2>
 
-                {/* ICONS AREA */}
-                {/* <Grid2
+            {/* Carousel AREA */}
+            <Grid2 container justifyContent={"center"} marginY={2}>
+                <Caroussel Photos={room.photos} />
+            </Grid2>
+
+            {/* Description AREA */}
+            <Grid2 alignItems={"center"} container marginY={2}>
+                <Grid2
                     sx={{
                         textAlign: {
                             xs: "center",
                             md: "start",
                         },
                     }}
-                    container justifyContent={"center"} marginY={2}>
-                    <Typography className={clsx(
-                        linkStyles({ color: "foreground" }),
-                        "!text-3xl",
-                    )} variant="h3">
-                        some of our accommodations
+                    direction={"column"}
+                    md={5}
+                >
+                    <Typography
+                        className={clsx(
+                            linkStyles({ color: "foreground" }),
+                            "!text-4xl",
+                            "mb-2"
+                        )}
+                        variant="h3"
+                    >
+                        Description
                     </Typography>
-                </Grid2> */}
-
-                <Grid2 alignItems={"center"} container marginY={2}>
-                    <Grid2
+                    <Typography
                         sx={{
                             textAlign: {
                                 xs: "center",
                                 md: "start",
                             },
                         }}
-                        direction={"column"}
-                        md={5}
+                        className={clsx(
+                            linkStyles({ color: "foreground" }), "text-center md:text-justify")}
+                        variant="body1"
                     >
-                        <Typography
-                            className={clsx(
-                                linkStyles({ color: "foreground" }),
-                                "!text-4xl",
-                                "mb-2"
-                            )}
-                            variant="h3"
-                        >
-                            Description
-                        </Typography>
-                        <Typography
-                            sx={{
-                                textAlign: {
-                                    xs: "center",
-                                    md: "start",
-                                },
-                            }}
-                            className={clsx(
-                                linkStyles({ color: "foreground" }), "text-center md:text-justify")}
-                            variant="body1"
-                        >
-                            {room.description}
-                        </Typography>
-                    </Grid2>
-
-                    {/* Divider */}
-                    <Grid2 md={2} display="flex" justifyContent="center">
-                        <Divider orientation="vertical" />
-                    </Grid2>
-
-                    {/* Form AREA */}
-                    <Grid2 justifyContent={"center"} spacing={2} md={5} marginY={2} container>
-                        <Grid2 textAlign={"center"} md={6}>
-                            <RangeCalendar
-                                aria-label="Date (Controlled)"
-                                value={value}
-                                onChange={setValue}
-                            />
-                        </Grid2>
-                        <Grid2 textAlign={"center"} alignContent={"center"} md={6}>
-                            <NextLink href="/booking">
-                                <Button
-                                    size="lg"
-                                    radius="lg"
-                                    className={clsx(
-                                        linkStyles({ color: "foreground" }),
-                                        "data-[active=true]:text-primary data-[active=true]:font-medium",
-                                        "uppercase"
-                                    )}
-                                    color="warning">
-                                    Book Now
-                                </Button>
-                            </NextLink>
-                        </Grid2>
-                    </Grid2>
+                        {room.description}
+                    </Typography>
                 </Grid2>
 
-                {/* Comments AREA */}
-                <Grid2 marginY={2} container>
-
+                {/* Divider */}
+                <Grid2 md={2} display="flex" justifyContent="center">
+                    <Divider orientation="vertical" />
                 </Grid2>
 
-                {/* Map AREA */}
-                <Grid2 marginY={2}>
-                    {/* Load map component only on client side */}
-                    <DynamicMap roomTitle={room.title} marker={room.marker} />
+                {/* Form AREA */}
+                <Grid2 justifyContent={"center"} spacing={2} md={5} marginY={2} container>
+                    <Grid2 textAlign={"center"} md={6}>
+                        <RangeCalendar
+                            aria-label="Date (Controlled)"
+                            value={value}
+                            onChange={setValue}
+                        />
+                    </Grid2>
+                    <Grid2 textAlign={"center"} alignContent={"center"} md={6}>
+                        <NextLink href="/booking">
+                            <Button
+                                size="lg"
+                                radius="lg"
+                                className={clsx(
+                                    linkStyles({ color: "foreground" }),
+                                    "data-[active=true]:text-primary data-[active=true]:font-medium",
+                                    "uppercase"
+                                )}
+                                color="warning">
+                                Book Now
+                            </Button>
+                        </NextLink>
+                    </Grid2>
                 </Grid2>
-            </>
-        );
-    }
+            </Grid2>
+
+            {/* Comments AREA */}
+            {/* <Grid2 marginY={2} container>
+            </Grid2> */}
+
+            {/* Map AREA */}
+            <Grid2 marginY={2}>
+                {/* Load map component only on client side */}
+                <DynamicMap roomTitle={room.title} marker={room.marker} />
+            </Grid2>
+        </>
+    );
 };
 
 interface RoomComponentProps {
